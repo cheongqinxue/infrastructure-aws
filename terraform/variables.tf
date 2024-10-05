be3d2bb6a57e2a216d3140b740822f98b13b557a
cheongqinxue <qinxue2001@hotmail.com>
2024-10-05 16:26:54 +0800

    remove spellcheck file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,13 +2,3 @@ variable "aws_region" {
     type = string
     description = "AWS region"
 }
-
-variable "github_username" {
-  type        = string
-  description = "Your GitHub username"
-}
-
-variable "github_token" {
-  type        = string
-  description = "Your GitHub token"
-}
