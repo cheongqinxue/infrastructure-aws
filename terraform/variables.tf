0543c29c1c8d89e22d44d16e74a19a13ced8b7df
cheongqinxue <qinxue2001@hotmail.com>
2024-10-05 17:21:21 +0800

    configure creds

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,13 +10,3 @@ variable "local_dev_keypair" {
   type = string
   description = "Local dev keypair"
 }
-
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-  description = "AWS_ACCESS_KEY_ID"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-  description = "AWS_SECRET_ACCESS_KEY"
-}
