230a9bbd53309431f9008d457d3d0dd3eb934815
cheongqinxue <qinxue2001@hotmail.com>
2024-10-05 22:49:17 +0800

    change availability zone, add ebs and subnet

--- terraform/modules/kit/main.tf
+++ terraform/modules/kit/main.tf
@@ -25,14 +25,31 @@ resource "aws_security_group" "kit_security_group" {
   }
 }
 
+# Define the VPC and Subnet to use
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_subnet_ids" "default_subnet_ids" {
+  vpc_id = data.aws_vpc.default_vpc.id
+}
+
 # Define an EC2 instance
 resource "aws_instance" "kit_ec2" {
   ami           = "ami-0ad522a4a529e7aa8" # Linux
   instance_type = "t2.micro"              # Free tier instance type
 
-  availability_zone = var.aws_region
+  availability_zone = "ap-southeast-1a"
   key_name          = var.local_dev_keypair
   security_groups   = [aws_security_group.kit_security_group.name]
+
+  subnet_id       = data.aws_subnet_ids.default_subnet_ids.ids[0]
+
+  # Root EBS Block Device
+  root_block_device {
+    volume_size = 30 # Size in GB
+    volume_type = "gp2" # General Purpose SSD
+  }
 
   tags = {
     Name = "kit-ec2"
