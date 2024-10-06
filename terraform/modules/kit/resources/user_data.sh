d8dcd03343307ae43ae338e696656d3d44ddeb39
cheongqinxue <qinxue2001@hotmail.com>
2024-10-06 12:48:05 +0800

    install nginx

--- terraform/modules/kit/resources/user_data.sh
+++ terraform/modules/kit/resources/user_data.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+
 # Install SSM Agent
 yum install -y amazon-ssm-agent
 
 # Start and enable the SSM Agent
 systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
+
+# Install NGINX
+# Update the package list
+sudo yum update -y
+
+# Install NGINX
+sudo amazon-linux-extras install nginx1.12 -y
+
+# Start NGINX
+sudo systemctl start nginx
+
+# Ensure NGINX starts on boot
+sudo systemctl enable nginx
